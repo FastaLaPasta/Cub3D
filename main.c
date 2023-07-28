0c5ee0f1e216d8929e24bed01f70607a25635bf7
Jason Giamporcaro <jgiampor@k2r1p6.42mulhouse.fr>
2023-07-28 12:29:42 +0200

    Parsing finis

--- main.c
+++ main.c
@@ -6,93 +6,21 @@
 /*   By: jgiampor <jgiampor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 13:43:26 by sboulogn          #+#    #+#             */
-/*   Updated: 2023/07/27 16:02:08 by jgiampor         ###   ########.fr       */
+/*   Updated: 2023/07/28 12:28:43 by jgiampor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cube.h"
 #define WIDTH 512
 #define HEIGHT 512
-#include "cube.h"
 
-// static mlx_image_t* image;
-
-// // -----------------------------------------------------------------------------
-
-// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-// {
-//     return (r << 24 | g << 16 | b << 8 | a);
-// }
-
-// void ft_randomize(void* param)
-// {
-// 	(void)param;
-// 	for (uint32_t i = 0; i < image->width; ++i)
-// 	{
-// 		for (uint32_t y = 0; y < image->height; ++y)
-// 		{
-// 			// uint32_t color = ft_pixel(
-// 			// 	rand() % 0xFF, // R
-// 			// 	rand() % 0xFF, // G
-// 			// 	rand() % 0xFF, // B
-// 			// 	rand() % 0xFF  // A
-// 			// );
-// 			mlx_put_pixel(image, i, y, -1228952577);
-// 		}
-// 	}
-// }
-
-// void ft_hook(void* param)
-// {
-// 	mlx_t* mlx = param;
-
-// 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-// 		mlx_close_window(mlx);
-// 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-// 		image->instances[0].y -= 15;
-// 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-// 		image->instances[0].y += 15;
-// 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-// 		image->instances[0].x -= 15;
-// 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-// 		image->instances[0].x += 15;
-// }
-
-// //-----------------------------------------------------------------------------
-
-int32_t main(int32_t argc, char **argv)
+int32_t	main(int32_t argc, char **argv)
 {
-	(void)argv;
+	t_map	map;
+
 	(void)argc;
-	//mlx_t* mlx;
-	t_map map;
-
 	if (fill_map_struct(&map, argv) == 1)
-		 return (1);
-	// Gotta error check this stuff
-	// if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-	// {
-	// 	puts(mlx_strerror(mlx_errno));
-	// 	return(EXIT_FAILURE);
-	// }
-	// if (!(image = mlx_new_image(mlx, 128, 128)))
-	// {
-	// 	mlx_close_window(mlx);
-	// 	puts(mlx_strerror(mlx_errno));
-	// 	return(EXIT_FAILURE);
-	// }
-	// if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	// {
-	// 	mlx_close_window(mlx);
-	// 	puts(mlx_strerror(mlx_errno));
-	// 	return(EXIT_FAILURE);
-	// }
-	
-	// mlx_loop_hook(mlx, ft_randomize, mlx);
-	// mlx_loop_hook(mlx, ft_hook, mlx);
-
-	// mlx_loop(mlx);
-	// mlx_terminate(mlx);
-	//system("leaks Game");
+		return (1);
 	ft_freemap(&map);
 	return (EXIT_SUCCESS);
 }
